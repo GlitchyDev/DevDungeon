e9b6b36c89b16b7b075c2613d20cbe5059a395df
GlitchyDev <RobertLouisHannah@gmail.com>
2017-12-23 23:05:27 -0500

    Horrible Attempt to fix room connecting issue
    Horrible Attempt

--- src/com/devotedworker/Map/Rooms/HallwayRoom.java
+++ src/com/devotedworker/Map/Rooms/HallwayRoom.java
@@ -12,83 +12,43 @@ import java.util.HashMap;
 import java.util.Random;
 
 public class HallwayRoom extends AbstractRoom {
-    private HallwayRoom connectedRoom;
-    private RoomDirection connectionDirection;
+    private HashMap<RoomDirection,HallwayRoom> connectedRooms;
+    private int treeID = -1;
 
     public HallwayRoom(Dungeon dungeon, RoomLocation roomlocation) {
         super(dungeon, roomlocation);
         roomType = RoomType.HALLWAY;
-
+        connectedRooms = new HashMap<>();
     }
 
-    public HallwayRoom(Dungeon dungeon, RoomLocation roomlocation, Random random) {
-        super(dungeon, roomlocation);
-        roomType = RoomType.HALLWAY;
-
+    public HashMap<RoomDirection,HallwayRoom> getPossibleConnections(Dungeon dungeon)
+    {
+        HashMap<RoomDirection,HallwayRoom> connectedRooms = new HashMap<>();
         for(RoomDirection direction: RoomDirection.getFloorRoomDirections())
         {
-            switch(random.nextInt(3))
-            {
-                case 0:
-                    roomOrientation.setDirectionConnection(direction,RoomConnection.NONE);
-                    break;
-                case 1:
-                    roomOrientation.setDirectionConnection(direction,RoomConnection.ABSOLUTE_ENTRANCE);
-                    break;
-                case 2:
-                    roomOrientation.setDirectionConnection(direction,RoomConnection.ABSOLUTE_ENTRANCE);
-                    break;
-            }
-        }
-    }
-
-
-    public HallwayRoom getConnectedRoom() {
-        return connectedRoom;
-    }
-    public void setConnectedRoom(HallwayRoom hallwayRoom, RoomDirection direction)
-    {
-        this.connectedRoom = hallwayRoom;
-        this.connectionDirection = direction;
-    }
-
-    public RoomDirection getConnectionDirection() {
-        return connectionDirection;
-    }
-
-    public boolean isRoomConnected(HallwayRoom hallwayRoom)
-    {
-        HallwayRoom currentRoom = connectedRoom;
-        while(true)
-        {
-            if(currentRoom == null)
-            {
-                return false;
-            }
-            else
-            {
-                if(currentRoom == hallwayRoom)
-                {
-                    return true;
+            if(dungeon.doesRoomExist(roomLocation.getDirectionLocation(direction))) {
+                if (dungeon.getRoom(roomLocation.getDirectionLocation(direction)).getRoomType() == RoomType.HALLWAY) {
+                    HallwayRoom room = (HallwayRoom) dungeon.getRoom(roomLocation.getDirectionLocation(direction));
+                    if (room.getTreeID() == -1 || room.getTreeID() != treeID) {
+                        connectedRooms.put(direction,room);
+                    }
                 }
-                currentRoom = currentRoom.getConnectedRoom();
             }
         }
+        return connectedRooms;
     }
 
-    public HallwayRoom getRoot()
-    {
-        HallwayRoom currentRoom = this;
-        while(true)
-        {
-            if(currentRoom.getConnectedRoom() == null)
-            {
-                return currentRoom;
-            }
-            else
-            {
-                currentRoom = currentRoom.getConnectedRoom();
-            }
-        }
+
+
+    public int getTreeID() {
+        return treeID;
+    }
+
+    public void setTreeID(int treeID) {
+        this.treeID = treeID;
+    }
+
+    public HashMap<RoomDirection, HallwayRoom> getConnectedRooms() {
+        return connectedRooms;
     }
 }
